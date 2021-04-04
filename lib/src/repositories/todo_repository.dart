39cb175c417ecfb3b7b708042104104aa60a8723
Mateus Araujo <mateus_1542@hotmail.com>
2021-04-04 15:16:13 -0300

    refactor(todos): improve TodoRepository legibility

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -5,21 +5,13 @@ class TodoRepository {
   late final Dio dio;
   static final url = 'https://jsonplaceholder.typicode.com/todos';
 
-  TodoRepository([Dio? client]) {
-    if (client == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
-  }
+  TodoRepository([Dio? client]) : dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
 
-    final list = (response.data as List)
-        .map((item) => TodoModel.fromJson(item))
+    return (response.data as List)
+        .map((json) => TodoModel.fromJson(json))
         .toList();
-
-    return list;
   }
 }
