376e50a476ef8b8380159f7fa539d1d5ee26a574
Mateus Araujo <mateus_1542@hotmail.com>
2021-04-04 17:46:36 -0300

    feat(todos): integrates view with controller

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,21 +1,22 @@
 import 'package:api_flow/src/models/todo_model.dart';
 import 'package:api_flow/src/repositories/todo_repository.dart';
+import 'package:flutter/material.dart';
 
 class HomeController {
   List<TodoModel> todos = [];
   late final TodoRepository _repository;
-  HomeState state = HomeState.start;
+  final state = ValueNotifier<HomeState>(HomeState.start);
 
   HomeController([TodoRepository? repository])
       : _repository = repository ?? TodoRepository();
 
   Future start() async {
-    state = HomeState.loading;
+    state.value = HomeState.loading;
     try {
       todos = await _repository.fetchTodos();
-      state = HomeState.success;
+      state.value = HomeState.success;
     } catch (e) {
-      state = HomeState.error;
+      state.value = HomeState.error;
     }
   }
 }
--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,19 +1,92 @@
+import 'package:api_flow/src/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  _success() {
+    return ListView.builder(
+      itemCount: controller.todos.length,
+      itemBuilder: (context, index) {
+        final todo = controller.todos[index];
+        return ListTile(
+          leading: Checkbox(
+            value: todo.completed,
+            onChanged: (bool? value) {},
+          ),
+          title: Text(todo.title!),
+        );
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          controller.start();
+        },
+        child: Text('Tentar novamente'),
+      ),
+    );
+  }
+
+  _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  _start() {
+    return Container();
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.success:
+        return _success();
+      default:
+        return _start();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.start();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('List Todos\'s'),
-        ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('item ${index + 1}'),
-            );
-          },
-        ));
+      appBar: AppBar(
+        title: Text('List Todos\'s'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh_outlined),
+            onPressed: () {
+              controller.start();
+            },
+          )
+        ],
+      ),
+      body: AnimatedBuilder(
+        animation: controller.state,
+        builder: (context, child) {
+          return stateManagement(controller.state.value);
+        },
+      ),
+    );
   }
 }
